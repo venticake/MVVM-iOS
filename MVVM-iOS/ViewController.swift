18aea1accabd8cd957688eacdebcf12143ed444a
bwjeong <bwjeong93@gmail.com>
2024-04-29 15:39:51 +0900

    first commit

---
+++ MVVM-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-iOS
+//
+//  Created by BYUNGWOOK JEONG on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
